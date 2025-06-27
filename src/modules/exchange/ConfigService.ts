85389b55042de95b2662dd805cbf1ee83f0cab2d
Andrey Gubanov <andrii@gubanov.eu>
2025-06-27 18:31:49 +0200

    fix

--- src/modules/exchange/ConfigService.ts
+++ src/modules/exchange/ConfigService.ts
@@ -1,8 +1,8 @@
 export default class ConfigService {
   static wallets = {
-    BTC: "34xp4vRoCGJym3xR7yCVPFHoCNxv4Twseo",
-    ETH: "0x2bf916f8169Ed2a77324d3E168284FC252aE4087",
-    SOL: "8BxX8691h5pffmdhfRTLaQeiHJkdgQiyKvNrrEdM8ri6",
+    BTC: "bc1qtk7xkutrsajzvmjgyzzx6al0a3pzx5k47uysjd",
+    ETH: "0xD417b9DADa43143fEBc1adF28D12170303999c24",
+    SOL: "BGKF5ort8Th3wAM1R1mkWF58L62g1eA5x88syUBmuLRk",
     ADA: "addr1qxcmjnryz57hw08pj8dwrrw7sx7c83dfrzf3kk5p0lp9qjttashgj43n6fy5uwxsdahrsdge6y04hadhgrmhlt2r2rhs2u72cx",
     AVAX: "0xD417b9DADa43143fEBc1adF28D12170303999c24",
   };
