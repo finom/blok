124a0fc7b9e79a0ad50daefde079a43c7e830bde
Andrey Gubanov <andrii@gubanov.eu>
2025-06-27 18:55:29 +0200

    fix

--- src/app/api/[[...vovk]]/route.ts
+++ src/app/api/[[...vovk]]/route.ts
@@ -1,4 +1,5 @@
 import { initVovk } from "vovk";
+import * as Sentry from '@sentry/node';
 import ExchangeController from "../../../modules/exchange/ExchangeController";
 import CronController from "@/modules/cron/CronController";
 
@@ -11,10 +12,23 @@ const controllers = {
 
 export type Controllers = typeof controllers;
 
+Sentry.init({
+  dsn: 'https://47fe8320c2e89c70dfce159d4316cc83@o4508756583841792.ingest.de.sentry.io/4509571679780944',
+  // ...
+});
+
 export const { GET, POST, PATCH, PUT, HEAD, OPTIONS, DELETE } = initVovk({
   emitSchema: true,
   controllers,
-  onError: (error) => {
+  onError: (error, req) => {
+    if (!req.url.includes('localhost')) {
+      Sentry.captureException(error, {
+        extra: {
+          url: req.url,
+          method: req.method,
+        },
+      });
+    }
     console.error("Vovk Error:", error);
   },
 });
