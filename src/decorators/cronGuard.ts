4fc62474a338d885999a15208cc8ced515ece288
Andrey Gubanov <andrii@gubanov.eu>
2025-06-27 18:20:20 +0200

    fix

--- src/decorators/cronGuard.ts
+++ src/decorators/cronGuard.ts
@@ -2,8 +2,6 @@ import { HttpException, HttpStatus, createDecorator } from "vovk";
 
 const cronGuard = createDecorator(async (req, next) => {
   const authHeader = req.headers.get("authorization");
-  console.log('CRON_SECRET:', process.env.CRON_SECRET, authHeader);
-
   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
     throw new HttpException(HttpStatus.UNAUTHORIZED, "Unauthorized");
   }
