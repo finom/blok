a98834a8c4c6651d0f1896f2d1b5482d24d0092d
Andrey Gubanov <andrii@gubanov.eu>
2025-06-27 17:54:05 +0200

    fix

--- src/app/api/[[...vovk]]/route.ts
+++ src/app/api/[[...vovk]]/route.ts
@@ -1,10 +1,12 @@
 import { initVovk } from "vovk";
 import ExchangeController from "../../../modules/exchange/ExchangeController";
+import CronController from "@/modules/cron/CronController";
 
 export const runtime = "nodejs";
 
 const controllers = {
   ExchangeRPC: ExchangeController,
+  _CronRPC: CronController,
 };
 
 export type Controllers = typeof controllers;
